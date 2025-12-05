e67b0a827caa5c8ccd16445304e7ddaaa9163a68
Vyanktesh Laxman Birdode <vyanktesh_birdode@circles.asia>
2025-12-05 10:03:38 +0530

    Vector Creation script has been added

--- converter.py
+++ converter.py
@@ -1,6 +1,17 @@
 from io import BytesIO
 from pypdf import PdfReader
 
+from langchain_openai import OpenAIEmbeddings
+from langchain_qdrant import QdrantVectorStore
+from qdrant_client import QdrantClient
+from qdrant_client.http.models import Distance, VectorParams
+from config import settings
+
+embeddings = OpenAIEmbeddings(model="text-embedding-3-large", api_key=settings.OPENAI_API_KEY)
+client = QdrantClient(path="qdrant_store")
+client.create_collection(collection_name="resumes", vectors_config=VectorParams(size=3072, distance=Distance.COSINE))
+client.close()
+
 def extract_text_from_pdf_bytes(pdf_bytes: bytes) -> str:
     reader = PdfReader(BytesIO(pdf_bytes))
     pages = []
